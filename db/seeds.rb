8b252ed7c0367d717e8bc615aa53394e0647cf1a
Frantz Hedrich <frantzhed@gmail.com>
2020-11-12 16:03:48 +0100

    Index and show for cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Bloody")
+Cocktail.create(name: "Jack")
+Cocktail.create(name: "Sunrise")
+
+Dose.create(description: "5cl", ingredient_id: 1, cocktail_id: 1)
+Dose.create(description: "5cl", ingredient_id: 2, cocktail_id: 1)
+Dose.create(description: "20cl", ingredient_id: 2, cocktail_id: 2)
+Dose.create(description: "40cl", ingredient_id: 3, cocktail_id: 3)
+
